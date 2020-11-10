7b6826bc22532377ef38ad543e2738d0187ea7d5
Exr0n <mail@exr0n.com>
2020-11-10 15:34:45 -0800

    init commit

---
+++ main.py
@@ -0,0 +1,27 @@
+
+def dist(a, b):
+    from math import sqrt
+    return sqrt(sum([(a[i] - b[i])**2 for i in range(len(a))]))
+
+def proc_data(fname: str, center: (float, float)):
+    dists = []
+    with open(fname, 'r', newline='') as csvfile:
+        from csv import reader
+        from math import sqrt
+        rf = reader(csvfile)
+        for i, row in enumerate(rf):
+            if i == 0: continue
+            if not row[0] or not row[1]: continue
+            dists.append(dist(center, [float(x) for x in row[:2]]))
+    return dists
+
+datasets = [
+    ('isoamyl_fly1.csv', (300, 300)),
+    ('isoamyl_fly2.csv', (300, 300)),
+        ]
+
+if __name__ == '__main__':
+    # from os import scandir
+    # for name in scandir('data'):
+    for fname, center in datasets:
+        print(fname, proc_data('data/'+fname, center))
